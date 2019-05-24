8ed91c901397d55e6b69ba5a48ac8a32d59aba99
jcvdav <juancarlos.villader@gmail.com>
2019-05-24 14:20:37 -0700

    Updated all figures and tables

--- scripts/geotiff_downloader.m
+++ scripts/geotiff_downloader.m
@@ -2,7 +2,7 @@ tic
 F1 = ftp('neoftp.sci.gsfc.nasa.gov'); %crear el ftp
 data_save = pwd(); %Directorio donde se guarda
 
-file = ['/geotiff.float/MY1DMM_CHLORA/*.FLOAT.TIFF'];
+file = ['/geotiff.float/MWOI_SST_M/*.FLOAT.TIFF'];
 mget(F1,file,data_save); % Se descarga el archivo con mget
 close(F1) %cierra el ftp
 toc
--- scripts/satesst.m
+++ scripts/satesst.m
@@ -3,7 +3,7 @@ function satesst(year)
 F1 = ftp('podaac-ftp.jpl.nasa.gov'); %crear el ftp
 data_save = pwd(); %Directorio donde se guarda
 
-file = ['/OceanTemperature/modis/L3/aqua/chlA/v2014.0/4km/monthly/',num2str(year),'/*.nc'];
+file = ['/OceanTemperature/modis/L3/aqua/4um/v2014.0/4km/monthly/',num2str(year),'/*.nc'];
 mget(F1,file,data_save); % Se descarga el archivo con mget
 close(F1) %cierra el ftp
 
@@ -13,5 +13,3 @@ end
 % ftp://podaac-ftp.jpl.nasa.gov/OceanTemperature/modis/L3/aqua/4um/v2014.0/4km/monthly/2003/
 % For Chla
 % ftp://podaac-ftp.jpl.nasa.gov/OceanTemperature/modis/L3/aqua/chlA/v2014.0/4km/monthly/2009/
-
-
